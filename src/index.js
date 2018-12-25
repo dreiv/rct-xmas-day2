998e2f84860e2aac5b8255cda6b1c2fdd705389d
Andrei Voicu <dreivoicu@gmail.com>
2018-12-25 13:13:48 +0200

    added pokemon

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function PokemonListItem({ className, component: Component = "li", ...props }) {
+function PokemonListItem({ className, component: Component = 'li', ...props }) {
   return (
     <Component
-      className={["pokemon-list-item", className].join(" ")}
+      className={['pokemon-list-item', className].join(' ')}
       {...props}
     />
-  );
+  )
 }
 
 let characterData = [
-  { name: "Pikachu" },
-  { name: "Bulbasaur" },
-  { name: "Charmander" }
-];
+  { name: 'Pikachu' },
+  { name: 'Bulbasaur' },
+  { name: 'Charmander' }
+]
 
 function App() {
+  characterData.push({ name: 'Eve' })
+
   return (
     <div>
       <h1>
@@ -26,13 +28,13 @@ function App() {
         : Day 2
       </h1>
       <ul>
-        {characterData.map(item => (
-          <PokemonListItem>{item.name}</PokemonListItem>
+        {characterData.map((item, idx) => (
+          <PokemonListItem key={idx}>{item.name}</PokemonListItem>
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
